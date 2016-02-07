92ded2e9e1931b89b2346df730c9e27ba6f45364
Lucas Nadalutti <lucasnadalutti@gmail.com>
2016-02-07 15:46:02 -0200

    Improvements

--- calc.js
+++ calc.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+    $(this).keydown(function(event) {
+        if (event.keyCode == 9) {
+            var $player1Score = $('#player1-score');
+            var $player2Score = $('#player2-score');
+            event.preventDefault();
+            if ($player1Score.is(':focus')) {
+                $player2Score.focus();
+            }
+            else {
+                $player1Score.focus();
+            }
+
+        }
+    });
+
     $('#choose-front').click(function() {
         $('#scores').data('game', 'front');
         $('#current-game').html('Dardo frontal');
@@ -14,6 +29,18 @@ $(document).ready(function() {
         $('#player2-total').html(0);
     });
 
+    $('#player1-score').keypress(function(event){
+        if(event.keyCode == 13){
+            $('#submit-player1-score').click();
+        }
+    });
+
+    $('#player2-score').keypress(function(event){
+        if(event.keyCode == 13){
+            $('#submit-player2-score').click();
+        }
+    });
+
     $('#submit-player1-score').click(function() {
         calcScore(1);
     });
@@ -26,6 +53,8 @@ $(document).ready(function() {
         var $scoresDiv = $('#scores');
         var submittedScore = parseInt($('#player'+playerNumber+'-score').val());
         var totalScore = parseInt($('#player'+playerNumber+'-total').html());
+
+        $('#player'+playerNumber+'-score').val('');
 
         if ($scoresDiv.data('game') === 'front') {
             if (totalScore - submittedScore < 0) {
